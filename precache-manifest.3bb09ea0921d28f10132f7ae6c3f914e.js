c3f602c2a7853b1899e44cb3525cc299b41882b2
pawel <pawel.niechoda@gmail.com>
2019-06-19 09:59:43 +0300

    Updates

---
+++ precache-manifest.3bb09ea0921d28f10132f7ae6c3f914e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8fcf492ef1c410a538a4fa6748f897",
+    "url": "/opening-times/index.html"
+  },
+  {
+    "revision": "0b6a06fbcbc2290f3aad",
+    "url": "/opening-times/static/css/main.44ae9c7d.chunk.css"
+  },
+  {
+    "revision": "bf93d2f03af7c835f624",
+    "url": "/opening-times/static/js/2.5a8d0baf.chunk.js"
+  },
+  {
+    "revision": "b51fcf1da10df9b17e0c",
+    "url": "/opening-times/static/js/3.9646a642.chunk.js"
+  },
+  {
+    "revision": "0b6a06fbcbc2290f3aad",
+    "url": "/opening-times/static/js/main.aa254511.chunk.js"
+  },
+  {
+    "revision": "755a301789740fd39877",
+    "url": "/opening-times/static/js/runtime~main.784f04ca.js"
+  },
+  {
+    "revision": "ca1c4643cffa56fa750e5d4aabb14b47",
+    "url": "/opening-times/static/media/clock.ca1c4643.svg"
+  }
+]);
